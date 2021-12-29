c31b8e08350dd910337a4a30829b38916e892f28
magnusjjj <magnusjjj@gmail.com>
2021-12-29 11:49:35 +0100

    Added in a toolbar with a single button to launch the connection.

---
+++ src/ToolBar/TheToolbar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace VSRemoteDebugger.ToolBar
+{
+    [ProvideToolboxControl("VSRemoteDebugger.ToolBar.TheToolbar", false)]
+    public partial class TheToolbar : UserControl
+    {
+        public TheToolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format(CultureInfo.CurrentUICulture, "We are inside {0}.Button1_Click()", this.ToString()));
+        }
+    }
+}
